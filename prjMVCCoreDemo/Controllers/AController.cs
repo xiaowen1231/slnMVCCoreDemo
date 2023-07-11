1db6507ef622e44c2b2987067aea428db8c6cba0
xiaowen1231 <shiao1124@gmail.com>
2023-07-11 13:26:45 +0800

    20230711

--- prjMVCCoreDemo/Controllers/AController.cs
+++ prjMVCCoreDemo/Controllers/AController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using prjMauiDemo.Models;
+using prjMVCCoreDemo.Models;
+using System.Text.Json;
 
 namespace prjMVCCoreDemo.Controllers
 {
@@ -24,6 +26,38 @@ namespace prjMVCCoreDemo.Controllers
         {
             ViewBag.id = id;
             return View();
+        }
+
+        public IActionResult showCountBySession()
+        {
+            int count = 0;
+            if (HttpContext.Session.Keys.Contains("count"))
+            {
+                count = (int)HttpContext.Session.GetInt32("count");
+            }
+            count++;
+            HttpContext.Session.SetInt32("count", count);
+            ViewBag.count = count;
+            return View();
+        }
+        public string demoObject2Json()
+        {
+            TCustomer customer = new TCustomer();
+            customer.FId = 1;
+            customer.FName = "John";
+            customer.FPhone = "0912345678";
+            customer.FEmail = "john@gmail.com";
+            customer.FAddress = "台北市";
+            customer.FPassword = "1234";
+
+            string json = JsonSerializer.Serialize(customer);
+            return json;
+        }
+        public string demoJson2Object()
+        {
+            string json = demoObject2Json();
+            TCustomer customer = JsonSerializer.Deserialize<TCustomer>(json);
+            return customer.FName + "/" + customer.FEmail;
         }
     }
 }
